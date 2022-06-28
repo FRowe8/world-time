cc6c03fab967818b3a9fa716fdd4581cff32320b
FRowe8 <frankrowe1@hotmail.co.uk>
2022-06-28 07:33:59 +0100

    feat: display time for timezone

--- src/components/TimezoneItem.vue
+++ src/components/TimezoneItem.vue
@@ -2,11 +2,18 @@
 import type { Timezone } from '../types'
 const { timezone } = defineProps<{ timezone: Timezone }>()
 
-const state = $computed(() => timezone.name.split('/')[0])
-const city = $computed(() => timezone.name.split('/')[1])
+const formatter = new Intl.DateTimeFormat('en-US', {
+  timeZone: timezone.name,
+  hour: 'numeric',
+  minute: 'numeric',
+})
+
+const state = $computed(() => timezone.name.split('/')[0].replace(/_/g, ' '))
+const city = $computed(() => timezone.name.split('/')[1].replace(/_/g, ' '))
 const offset = $computed(() =>
   timezone.offset > 0 ? `+${timezone.offset}` : timezone.offset,
 )
+const time = $computed(() => formatter.format(now.value))
 </script>
 
 <template>
@@ -17,10 +24,14 @@ const offset = $computed(() =>
     <div flex="~ col" text-left flex-auto>
       <div>
         {{ city }}
+        <sup border="~ base rounded" px1>{{ timezone.abbr }}</sup>
       </div>
       <div text-sm op50 leading-1em>
         {{ state }}
       </div>
+    </div>
+    <div tabular-nums>
+      {{ time }}
     </div>
   </div>
 </template>
